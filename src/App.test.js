3a5ec921654b77fdb459ff9fca9f784f26cfb467
Akshay Kadam <akshaykadam0904@gmail.com>
2019-12-05 20:26:16 +0530

    Make counter app & perform E2E 😜

--- src/App.test.js
+++ src/App.test.js
@@ -11,20 +11,34 @@ beforeAll(async () => {
   await page.goto("http://localhost:3000/");
 });
 
-test("renders learn react link", async () => {
-  await page.waitForSelector(".App");
+test("renders counter", async () => {
+  await page.waitForSelector(".header");
 
-  const header = await page.$eval(".App-header>p", e => e.innerHTML);
-  expect(header).toBe(`Edit <code>src/App.js</code> and save to reload.`);
+  const header = await page.$eval(".header", e => e.innerHTML);
+  expect(header).toBe("Counter");
+});
 
-  const link = await page.$eval(".App-header>a", e => {
-    return {
-      innerHTML: e.innerHTML,
-      href: e.href
-    };
-  });
-  expect(link.innerHTML).toBe(`Learn React`);
-  expect(link.href).toBe("https://reactjs.org/");
+test("sets initial state to zero", async () => {
+  await page.waitForSelector(".counter-app");
+
+  const count = await page.$eval(".count", e => e.innerHTML);
+  expect(count).toBe("0");
+});
+
+test("increments counter by 1", async () => {
+  await page.waitForSelector(".counter-app");
+
+  await page.click(".increment");
+  const count = await page.$eval(".count", e => e.innerHTML);
+  expect(count).toBe("1");
+});
+
+test("decrements counter by 1", async () => {
+  await page.waitForSelector(".counter-app");
+
+  await page.click(".decrement");
+  const count = await page.$eval(".count", e => e.innerHTML);
+  expect(count).toBe("0");
 });
 
 afterAll(() => {
